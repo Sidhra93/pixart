c9479d5fdd91d21e1ddccca9ccb280e9f3727c0d
Sidhra <sidhra.cader@hotmail.com>
2017-10-20 16:23:34 +1100

    stamp tool functionality

--- pixart.js
+++ pixart.js
@@ -1,40 +1,36 @@
 var $colorInput = $('#color-field');
 var $setColorBtn = $('#set-color');
-var option =
-$('input[name=option]:checked').val()
 
 for (var i = 1; i <= 10000; i++) {
   $div = $('<div>').addClass("square");
   $('body').append($div);
 }
 
-// if (option === "paint-brush") {
-//   $setColorBtn.on('click', function(event){
-//     event.preventDefault();
-//     var color = $colorInput.val();
-//     $('.brush').css('background', color);
-//
-    // $('body').on('mouseover', '.square', function(event){
-    //   $(event.target).css('background', color);
-    // });
-//   })
-// } else {
-//
-// }
 $setColorBtn.on('click', function(){
   event.preventDefault();
-  var poster = $colorInput.val();
-  var options = {
-    url: "http://www.omdbapi.com",
-    method: "get",
-    data: {t: poster, apikey: '2f6435d9'}
-  }
-  $.ajax(options).done(function(response){
-    // console.log(response.Poster);
-    $('body').on('mouseover', '.square', function(event){
-      $(event.target).css("background', 'url('"+ response.Poster +"')");
-    });
+  var option = $('input[name=option]:checked').val();
+  if (option === "paint-brush") {
+    var color = $colorInput.val();
+    $('.brush').css('background', color);
 
-  })
+    $('body').on('mouseover', '.square', function(event){
+      $(event.target).css('background', color);
+    });
+  } else if (option === "stamp-tool") {
+    var poster = $colorInput.val();
+    var options = {
+      url: "http://www.omdbapi.com",
+      method: "get",
+      data: {t: poster, apikey: '2f6435d9'}
+    }
+    $.ajax(options).done(function(response){
+      // console.log(response.Poster);
+      $('.brush').css('background-image', 'url('+ response.Poster + ')');
+
+      $('body').on('mouseover', '.square', function(event){
+        $(event.target).css('background-image', 'url('+ response.Poster + ')');
+      });
+    });
+  }
 
 });
