6fbe8ac2130229b525430396c681afff9b565c53
Sidhra <sidhra.cader@hotmail.com>
2017-10-20 15:44:32 +1100

    Stamp Tool Functionality

--- pixart.js
+++ pixart.js
@@ -1,18 +1,40 @@
 var $colorInput = $('#color-field');
 var $setColorBtn = $('#set-color');
-
-$setColorBtn.on('click', function(event){
-  event.preventDefault();
-  var color = $colorInput.val();
-  $('.brush').css('background', color);
-})
+var option =
+$('input[name=option]:checked').val()
 
 for (var i = 1; i <= 10000; i++) {
   $div = $('<div>').addClass("square");
   $('body').append($div);
 }
 
-$('body').on('mouseover', '.square', function(event){
-  var color = $colorInput.val();
-  $(event.target).css('background', color);
+// if (option === "paint-brush") {
+//   $setColorBtn.on('click', function(event){
+//     event.preventDefault();
+//     var color = $colorInput.val();
+//     $('.brush').css('background', color);
+//
+    // $('body').on('mouseover', '.square', function(event){
+    //   $(event.target).css('background', color);
+    // });
+//   })
+// } else {
+//
+// }
+$setColorBtn.on('click', function(){
+  event.preventDefault();
+  var poster = $colorInput.val();
+  var options = {
+    url: "http://www.omdbapi.com",
+    method: "get",
+    data: {t: poster, apikey: '2f6435d9'}
+  }
+  $.ajax(options).done(function(response){
+    // console.log(response.Poster);
+    $('body').on('mouseover', '.square', function(event){
+      $(event.target).css("background', 'url('"+ response.Poster +"')");
+    });
+
+  })
+
 });
